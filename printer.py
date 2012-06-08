4959c01d88d2b65368b541279c90bce1129055d3
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2012-06-08 18:12:39 +0100

    First pass at port of freerange's printer.ino

---
+++ printer.py
@@ -0,0 +1,57 @@
+import serial
+import time
+import urllib
+
+RASPI_SERIAL = '/dev/ttyAMA0'
+DEBUG_SERIAL = '/dev/master'
+
+printerDevice = DEBUG_SERIAL
+printerId = None
+printer   = None
+
+printerHost = "http://printer.gofreerange.com:80"
+
+def getPrinterId():
+    return "1234abc"
+
+def initSettings():
+    print "init settings"
+
+    global printerId
+    printerId = getPrinterId()
+
+def initPrinter():
+    global printer
+    printer = serial.Serial(printerDevice, 19200)
+
+def getPrinterUrl():
+    return printerHost + "/printer/" + printerId
+
+def checkForDownload():
+    url = getPrinterUrl()
+    print "Check for download: " + url
+    response = urllib.urlopen(url)
+    status = str( response.getcode() )
+
+    print "Status: " + status
+
+    if status == '200':
+        print "has response"
+        sendToPrinter(response)
+    else:
+        print "got response: " + status
+        time.sleep(5)
+
+def sendToPrinter(f):
+    bytes = f.read()
+    for b in bytes:
+        printer.write(b)
+    printer.flush()
+
+initSettings()
+initPrinter()
+print printerId
+print printerDevice
+
+#while(True):
+checkForDownload()
