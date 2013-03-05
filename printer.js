0552c33f4c5d14de78e66ada3018ab78c4bffc7f
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-03-05 15:03:27 +0000

    Script to run printer controller

---
+++ printer.js
@@ -0,0 +1,19 @@
+PrintQueue  = require("./lib/print-queue").PrintQueue
+PrintServer = require("./lib/print-server.js").PrintServer
+PrinterId   = require("./lib/printer-id.js").PrinterId
+Printer     = require("./lib/printer.js").Printer
+Ui          = require("./lib/ui").Ui
+
+PrinterController = require("./lib/printer-controller.js").PrinterController
+
+config = {
+  type   : 'A2-raw',
+  server : 'http://printer.gofreerange.com:80/printer/' + PrinterId.get()
+}
+
+controller = new PrinterController({
+                    printServer: new PrintServer(config.server, config.type), 
+                    printQueue: new PrintQueue(),
+                    ui: new Ui(),
+                    printer: new Printer('/dev/ttyAMA0')
+                  })
